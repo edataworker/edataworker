9a020ad561573f17f4d7017d0aa1c9d8f47d9390
edataworker <edataworker@gmail.com>
2025-12-22 21:31:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-const username = "YOUR_GITHUB_USERNAME";
-const repo = "YOUR_REPOSITORY_NAME";
-const folder = "logos";
+const username = "edataworker";
+const repo = "mysite";
+const branch = "main";
 
-const gallery = document.getElementById("logoGallery");
+// LOGOS
+loadImages("logos", "logoGallery");
 
-fetch(`https://api.github.com/repos/${username}/${repo}/contents/${folder}`)
-  .then(res => res.json())
-  .then(files => {
-    files.forEach(file => {
-      if (file.type === "file" && /\.(png|jpg|jpeg)$/i.test(file.name)) {
-        const img = document.createElement("img");
-        img.src = file.download_url;
-        img.loading = "lazy";
-        gallery.appendChild(img);
-      }
-    });
-  })
-  .catch(err => console.error("Error loading logos:", err));
+// TESTIMONIALS
+loadImages("testimonials", "testimonialGallery");
+
+
+function loadImages(folder, elementId) {
+  const gallery = document.getElementById(elementId);
+
+  fetch(`https://api.github.com/repos/${username}/${repo}/contents/${folder}?ref=${branch}`)
+    .then(res => res.json())
+    .then(files => {
+      files.forEach(file => {
+        if (file.type === "file" && /\.(png|jpg|jpeg)$/i.test(file.name)) {
+          const img = document.createElement("img");
+          img.src = file.download_url;
+          img.loading = "lazy";
+          gallery.appendChild(img);
+        }
+      });
+    })
+    .catch(err => console.error("Error loading " + folder, err));
+}
